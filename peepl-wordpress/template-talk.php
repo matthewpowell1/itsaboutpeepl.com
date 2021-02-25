ef69076f463fd77da40a17050c1db3cacda1d240
Jordan Kabyemela <wyzig34@gmail.com>
2021-02-25 21:33:44 +0000

    padding

--- peepl-wordpress/template-talk.php
+++ peepl-wordpress/template-talk.php
@@ -12,7 +12,7 @@ visibility: hidden;
 }</style>
 
 <div class="wp-block-cover alignfull has-background-dim has-background-gradient has-red-to-orange-gradient-background pb-0" style="min-height:400px;">
-    <div class="wp-block-cover__inner-container">
+    <div class="">
         <h2 class="alignfull has-text-align-center has-huge-font-size"><?php the_field('header'); ?></h2>
     </div> 
 </div>
