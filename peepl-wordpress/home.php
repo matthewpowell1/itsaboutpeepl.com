56275f861b047f557c9d328d34e902b4ac5f0e85
Matthew <garyjuliepowell@gmail.com>
2022-01-19 16:33:07 +0000

    update site to new theme

---
+++ peepl-wordpress/home.php
@@ -0,0 +1,114 @@
+<?php /* Template Name: Peepl Blog */ ?>
+
+<?php get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
+    <div class="hero wp-block-group alignfull">
+        <div class="wp-block-group__inner-container">
+            <div class="wp-block-group alignwide" style="padding-right: 24%;">
+                <div class="wp-block-group__inner-container">
+                    <h2 class="alignwide has-text-align-left has-black-color has-text-color">Find out more about what interests and inspires Peepl</h2>
+                    <p class="has-black-color has-text-color has-large-font-size">Here you can find blogs written by the Peepl team, our shareholders and from the wider Liverpool, and international, community.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wp-block-group alignfull">
+        <div class="wp-block-group__inner-container">
+            <div class="wp-block-columns alignwide">
+                <div class="wp-block-column">
+                    <main class="blog-grid extrawide">
+                        <article class="new-article-heading">New article</article>
+                        <article class="featured-article-heading">A favourite from the archive</article>
+                    </main>
+            </div>
+    </div>
+
+    <div class="wp-block-columns alignwide">
+        <div class="wp-block-column">
+            <main class="blog-grid extrawide">
+                <article class="new-article">
+                    <h5 class="new-article-image-date">15th January 2021</h5>
+                    <h3 class="new-article-title">Web 3.0 explained</h3>
+                    <h4 class="new-article-image-author">by Zarino Zappia</h4>
+                </article>
+                <article class="featured-article">
+                    <h5 class="featured-article-image-date">29th December 2021</h5>
+                    <h3 class="featured-article-title">Enjoy the little things</h3>
+                    <h4 class="featured-article-image-author">by Leon Rossitor</h4>
+                </article>
+            </main>
+        </div>
+    </div>
+
+    <div class="wp-block-columns alignwide">
+        <div class="wp-block-column">
+            <main class="secondary-grid extrawide">
+                <article class="related-article-image"></article>
+                <article class="related-article-title">
+                    <h3>7 Air-Cleaning Tropicals you need in your space</h3>
+                    <p class="related-article-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#">Read more</a></p>
+                </article>
+            </main>
+            <main class="secondary-grid extrawide">
+                <article class="related-article-image"></article>
+                <article class="related-article-title">
+                    <h3>Why Wireframing is an Essential Skill for More Than Just Designers!</h3>
+                    <p class="related-article-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#">Read more</a></p>
+                </article>
+            </main>
+        </div>
+    </div>
+    </div></div>
+
+    <div class="wp-block-group is-style-default">
+        <div class="wp-block-group__inner-container">
+            <div class="wp-block-buttons alignwide is-content-justification-center">
+                <div class="wp-block-button is-style-outline">
+                    <a class="wp-block-button__link has-black-color has-text-color">Load more</a>
+                </div>
+            </div>
+    </div></div>
+
+    <div class="wp-block-group alignfull">
+        <div class="wp-block-group__inner-container">
+            <div class="frm_forms  with_frm_style frm_style_formidable-style" id="frm_form_4_container" data-token="8ca6a7f758e43b1b3ad9adffa355c17b">
+                <form enctype="multipart/form-data" method="post" class="frm-show-form  frm_js_validate  frm_pro_form  frm_ajax_submit " id="form_newsletter-subscription" data-token="8ca6a7f758e43b1b3ad9adffa355c17b">
+                    <div class="frm_form_fields ">
+                        <fieldset>
+                            <legend class="frm_screen_reader">Newsletter subscription</legend>
+                            <div class="frm_fields_container">
+                                <input type="hidden" name="frm_action" value="create">
+                                <input type="hidden" name="form_id" value="4">
+                                <input type="hidden" name="frm_hide_fields_4" id="frm_hide_fields_4" value="">
+                                <input type="hidden" name="form_key" value="newsletter-subscription">
+                                <input type="hidden" name="item_meta[0]" value="">
+                                <input type="hidden" id="frm_submit_entry_4" name="frm_submit_entry_4" value="ec52c20135">
+                                <input type="hidden" name="_wp_http_referer" value="/new-blog/"><div id="frm_field_39_container" class="frm_form_field form-field form-field-style  frm_required_field frm_top_container frm_full">
+                                <label for="field_29yf4d2" id="field_29yf4d2_label" class="form-primary-label">Subscribe to our newsletter
+                                    <span class="frm_required" aria-hidden="true">*</span>
+                                </label>
+                                <input type="email" id="field_29yf4d2" name="item_meta[39]" value="" placeholder="Enter your email address" data-reqmsg="This field cannot be blank." aria-required="true" data-invmsg="Please enter a valid email address." aria-invalid="false">
+                            </div>
+                                <input type="hidden" name="item_key" value="">
+                                    <div class="frm_verify" aria-hidden="true">
+                                        <label for="frm_email_4">If you are human, leave this field blank.</label>
+                                        <input type="text" class="frm_verify" id="frm_email_4" name="frm_verify" value="">
+                                    </div>
+                                <input name="frm_state" type="hidden" value="eVEQ81eu50lhKDeLCl59xLYbbhhu5yzn7+1k1IxTlGI="><div class="wp-block-button has-custom-font-size is-style-outline has-medium-font-size">
+
+                                <button class="frm_button_submit frm_final_submit wp-block-button__link has-black-color has-text-color" type="submit" style="font-size: 16px !important" formnovalidate="formnovalidate">Subscribe</button>
+                            </div>
+                        </div>
+                        </fieldset>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
